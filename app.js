57a80b1a5347c18a7e45644a87037dcc0bcf289f
qbixmex <qbixmex@gmail.com>
2021-03-21 16:37:55 -0600

    Colors added to console

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
 const { createFileTable } = require("./helpers/multiply");
-const argv = require("yargs")
-  .option("b", {
-    alias: "base",
-    type: "number",
-    demandOption: true,
-  })
-  .option("l", {
-    alias: "listar",
-    type: "boolean",
-    demandOption: true,
-    default: false
-  })
-  .check((argv, options) => {
-    if (isNaN(argv.b)) {
-      throw "¡ La base tiene que ser un número !";
-    }
-    return true;
-  })
-  .argv;
+const argv = require("./config/yargs");
+require("colors");
 
 console.clear();
 
@@ -25,4 +8,4 @@ console.clear();
 
 createFileTable(argv.b, argv.l)
   .then(msg => console.log(msg))
-  .catch(error => console.log(error));
+  .catch(error => console.log(error.red));
--- helpers/multiply.js
+++ helpers/multiply.js
@@ -1,21 +1,22 @@
 const fileSystem = require("fs");
+const colors = require("colors");
 
 const createFileTable = async (base = 0, listar = false) => {
   try {
     let output = '';
-    output += '===================\n';
-    output += `${' '.repeat(4)}Tabla del ${base}\n`;
-    output += '===================\n';
+    output += '===================\n'.magenta;
+    output += `${' '.repeat(4)}Tabla del ${colors.blue(base)}\n`.white;
+    output += '===================\n'.magenta;
 
     for(let i = 1; i <= 10; i++) {
-      output += `${base} x ${i} = ${base * i}\n`;
+      output += `${colors.white(base)} ${'x'.blue} ${i} ${'='.cyan} ${colors.green(base * i)}\n`;
     }
 
     if(listar) console.log(output);
 
     fileSystem.writeFileSync(`tabla-del-${base}.txt`, output);
 
-    return `¡ tabla-del-${base}.txt ha sido creada correctamente !\n`;
+    return '!' + ` tabla-del-${base}.txt`.green + ` ha sido creada correctamente !\n`;
   } catch(error) {
     throw error;
   }
